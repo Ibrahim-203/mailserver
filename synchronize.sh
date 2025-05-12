7eb59d1282c8fec451fb8dc682d482a0d1f3d93c
vboxuser <vboxuser@monserveur.local>
2025-05-12 07:02:05 +0300

    Configuration avec roundcube

--- synchronize.sh
+++ synchronize.sh
@@ -4,4 +4,5 @@ cp /etc/postfix/main.cf ./postfix/
 cp /etc/postfix/master.cf ./postfix/
 cp /etc/dovecot/dovecot.conf ./dovecot/
 cp -r /etc/dovecot/conf.d/* ./dovecot/
+cp -r /etc/apache2/sites-available ./
 echo "Fichiers synchronisés."
